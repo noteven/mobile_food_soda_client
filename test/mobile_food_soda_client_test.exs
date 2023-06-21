d16b1095a7713dd24a81e9e77e84479c3a35b615
noteven <15650992+noteven@users.noreply.github.com>
2023-06-21 20:51:38 -0300

    Removed exploratory design tests since warnings kept failing CI.

--- test/mobile_food_soda_client_test.exs
+++ test/mobile_food_soda_client_test.exs
@@ -5,35 +5,6 @@ defmodule MobileFoodSodaClientTest do
 
   alias MobileFoodSodaClient.FacilityPermit
 
-  @csv_file "./support/Mobile_Food_Facility_Permit.csv"
-  @json_file "./support/Mobile_Food_Facility_Permit.json"
-
-  describe "client from file" do
-    @tag :skip
-    test "read CSV coded permits" do
-      # contents = File.stream!(@csv_file)
-      # {:ok, facility_permits} = MobileFoodSodaClient.fetch(contents, &CSV.decode/1)
-
-      # assert length(facility_permits) > 0
-
-      # Enum.each(facility_permits, fn permit ->
-      #  assert %FacilityPermit{} = permit
-      # end)
-    end
-
-    @tag :skip
-    test "read JSON coded permits" do
-      # contents = File.stream!(@json_file)
-      # {:ok, facility_permits} = MobileFoodSodaClient.fetch(contents, &Jason.decode/1)
-
-      # assert length(facility_permits) > 0
-
-      # Enum.each(facility_permits, fn permit ->
-      #  assert %FacilityPermit{} = permit
-      # end)
-    end
-  end
-
   describe "client from SODA API" do
     test "can fetch permits" do
       {:ok, facility_permits} = MobileFoodSodaClient.fetch()
